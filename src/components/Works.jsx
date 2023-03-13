04410f8d1de1ca157e3da170e060bd311b036a70
Fahad Kiani <fahadkiani@fahads-mbp.lan>
2023-03-13 01:15:37 -0400

    changes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -17,7 +17,7 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <motion.div variants={( "spring", index * 0.5, 0.75)}>
       <Tilt
         options={{
           max: 45,
@@ -76,14 +76,14 @@ const ProjectCard = ({
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <motion.div>
         <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
 
       <div className='w-full flex'>
         <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
+          // variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
           Following projects showcases my skills and experience through
